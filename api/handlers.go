c5bc4d90bf6dabffefeba8cdd2c5e7feaac148f6
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-02-04 03:17:49 +0530

    POST: append order to orderBook

--- api/handlers.go
+++ api/handlers.go
@@ -3,16 +3,19 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"sort"
 )
 
 type User struct {
-	userId  string
+	UserId  string
 	balance map[string]float64
 }
 
 type Order struct {
-	userId   string
+	UserId   string
+	Side     string
 	Price    float64
 	Quantity float64
 }
@@ -23,14 +26,15 @@ var bid []Order
 var ask []Order
 
 var users = []User{
-	{userId: "1", balance: map[string]float64{"BTC": 1.00, "USDT": 10000.00}},
-	{userId: "2", balance: map[string]float64{"BTC": 1.00, "USDT": 10000.00}},
+	{UserId: "1", balance: map[string]float64{"BTC": 1.00, "USDT": 10000.00}},
+	{UserId: "2", balance: map[string]float64{"BTC": 1.00, "USDT": 10000.00}},
 }
 
 // TODO: Implement incoming request handling
 func (s *Server) order(w http.ResponseWriter, r *http.Request) {
-	body := json.NewDecoder(r.Body)
-	body.Decode(&body)
+	body, _ := io.ReadAll(r.Body)
+	var order Order
+	json.Unmarshal(body, &order)
 
 	remainingQuantity := fulfillOrder()
 
@@ -38,21 +42,22 @@ func (s *Server) order(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Fulfilled"))
 	}
 
-	/* if body.side == "BID" {
-		order := Order{userId: body.userId, Price: body.price, Quantity: remainingQuantity}
-		bid = append(bid, order)
+	if order.Side == "BID" {
+		newOrder := Order{UserId: order.UserId, Price: order.Price, Quantity: remainingQuantity}
+		bid = append(bid, newOrder)
 
 		sort.Slice(bid, func(i, j int) bool {
 			return bid[i].Price > bid[j].Price
 		})
 	} else {
-		order := Order{userId: body.userId, Price: body.price, Quantity: remainingQuantity}
 		ask = append(ask, order)
 
 		sort.Slice(ask, func(i, j int) bool {
 			return ask[i].Price < ask[j].Price
 		})
-	} */
+	}
+
+	fmt.Println(ask)
 }
 
 // TODO: make an orderBook from all the orders
