c5cd32b7bcfc7f9468be96042451528a6cf9c8cd
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-02-16 11:46:00 +0530

    binance-ws connected

--- main.go
+++ main.go
@@ -4,12 +4,38 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/Kulvir-parmar/xchange/api"
+	"github.com/gorilla/websocket"
 )
+
+const wsendpoint = "wss://fstream.binance.com/stream?streams=btcusdt@depth"
+
+type BinanceOrderBook struct {
+	Asks [][]string `json:"a"`
+	Bids [][]string `json:"b"`
+}
+
+type BinanceDepthResponse struct {
+	Stream string           `json:"stream"`
+	Data   BinanceOrderBook `json:"data"`
+}
 
 func main() {
 	fmt.Println("Welcome to JJ Exchange")
 
-	server := api.NewServer(":3000")
-	log.Fatal(server.Start())
+	conn, _, err := websocket.DefaultDialer.Dial(wsendpoint, nil)
+	if err != nil {
+		log.Fatal("dial: ", err)
+	}
+
+	var result BinanceDepthResponse
+
+	for {
+		if err := conn.ReadJSON(&result); err != nil {
+			log.Fatal("read: ", err)
+			return
+		}
+
+		fmt.Printf("Bids: %v\n", result.Data.Bids)
+		fmt.Printf("Asks: %v\n", result.Data.Asks)
+	}
 }
